ef25364603a4fe24608411b54a7bd5fb11568c3b
student <student@10.14.0.186>
2017-08-17 10:09:49 +0530

    Hey how you doin?

--- src/sorting.java
+++ src/sorting.java
@@ -19,7 +19,7 @@ public class sorting {
     public static void main(String[] args) {  
                 int arr[] ={3,60,35,2,45,320,5};  
                  
-                System.out.println("Array Before Bubble Sort");  
+                System.out.println("Array Before Bubble Sorting");  
                 for(int i=0; i < arr.length; i++){  
                         System.out.print(arr[i] + " ");  
                 }  
@@ -27,7 +27,7 @@ public class sorting {
                   
                 bubbleSort(arr);//sorting array elements using bubble sort  
                  
-                System.out.println("Array After Bubble Sort");  
+                System.out.println("Array After Bubble Sorting");  
                 for(int i=0; i < arr.length; i++){  
                         System.out.print(arr[i] + " ");  
                 }  
